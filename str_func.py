a2bb9a22ed8b780e5d077690687ad172e3e78a83
Andrew Kurin <andrew_kurin@mail.ru>
2024-03-29 00:34:50 +0300

    added print_title and change print_upper

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def print_upper(str_in):
+    """Вся строка из заглавных букв"""
     return str_in.upper()
 
+
+def print_title(str_in):
+    """функция возвращает каждое слово с большой буквы"""
+    return str_in.title()

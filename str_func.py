7d36f7e126fbc59839434d61d17954e21381b05f
Andrew Kurin <andrew_kurin@mail.ru>
2024-03-29 01:19:59 +0300

    function return changed string, not printed!!!

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
-def print_upper(str_in):
+def ret_upper(str_in):
     """Вся строка из заглавных букв"""
     return str_in.upper()
 
 
-def print_title(str_in):
+def ret_title(str_in):
     """функция возвращает каждое слово с большой буквы"""
     return str_in.title()
